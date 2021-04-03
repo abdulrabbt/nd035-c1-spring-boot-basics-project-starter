e56fe065add971a05fbb7d98568b8cbe3368eec1
abdulrab <abdulrab.bt@hotmail.com>
2021-04-03 21:54:32 +0300

    create model for user and note

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/User.java
@@ -0,0 +1,61 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class User {
+    private Integer userId;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String salt;
+    private String password;
+
+    public User(Integer userId, String username, String firstName, String lastName, String salt, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salt = salt;
+        this.password = password;
+    }
+
+
+    public Integer getUserId() {
+        return userId;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getSalt() {
+        return salt;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
